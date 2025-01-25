cb3be8feea980d1acd1090d5d00fc8df79774689
wt29 <tglynn@hotmail.com>
2025-01-25 14:30:09 +1100

    dafaadf

--- include/kitchen.h
+++
@@ -1,9 +0,0 @@
-#define APARRAY {"LivingRoom","HackDesk","backdoor"}
-#define PASSARRAY {"BigFurryCat","BigFurryCat","BigFurryCat"}
-#define APCOUNT 3
-#define NODENAME "Kitchen"
-#define SENSORCOUNT 2
-#define HASDHT12
-#define IOTAWATT
-#define IW_VALUE "http://192.168.1.100/query?select=[time.local.iso,Mains.watts]&begin=m-1m&end=m&group=m&format=csv"
-#define IW_SOLAR "http://192.168.1.100/query?select=[time.local.iso,Solar.watts]&begin=m-1m&end=m&group=m&format=csv"
