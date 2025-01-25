b3c7c60ef8f6fcc5ac4e42e2cc01d6a62e01539d
wt29 <tglynn@hotmail.com>
2025-01-25 13:43:28 +1100

    Fixed

--- include/data.h
+++ include/data.h
@@ -1,1 +1,5 @@
-#include "external.h"
+#include "shed.h"
+#define HOST "192.168.1.50"
+#define MYAPIKEY "ad0c0c29c83ec863c8b600151c3da1c5"
+#define TIMESERVER "0.au.pool.ntp.org"
+#define TZOFFSET 36000     // time in seconds
