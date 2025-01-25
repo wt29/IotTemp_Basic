2af3eea04d145917b394cff84940322e4f8571f3
wt29 <tglynn@hotmail.com>
2025-01-25 14:34:09 +1100

    sdf

--- include/hackdesk.h
+++
@@ -1,9 +0,0 @@
-#define NODENAME "HackDesk"
-#define APARRAY {"chapman","HackDesk"}
-#define PASSARRAY {"BigFurryCat","BigFurryCat","BigFurryCat"}
-#define APCOUNT 2
-#define SENSORCOUNT 2
-#define SHT
-#define IOTAWATT
-#define IW_VALUE "http://192.168.1.100/query?select=[time.local.iso,Mains.watts]&begin=m-1m&end=m&group=m&format=csv"
-#define IW_SOLAR "http://192.168.1.100/query?select=[time.local.iso,Solar.watts]&begin=m-1m&end=m&group=m&format=csv"
