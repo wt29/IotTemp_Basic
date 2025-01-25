b5a38c11b9a66adce22a89cbc00790c4632af948
wt29 <tglynn@hotmail.com>
2025-01-25 13:51:13 +1100

    Dunno?

--- src/data.h
+++
@@ -1,6 +0,0 @@
-#include "hackdesk.h"
-#define HOST "192.168.1.50"
-#define MYAPIKEY "290c0dc3bf69a67ef6512feb5601b0ad"
-#define EC2MYAPIKEY "ad0c0c29c83ec863c8b600151c3da1c5"
-#define TIMESERVER "0.au.pool.ntp.org"
-#define TZOFFSET 36000     // time in seconds
